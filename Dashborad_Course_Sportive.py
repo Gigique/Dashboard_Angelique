bb0bc96d39dca254e249e8346705917528b419a3
Angélique <angelique.bidot01@gmail.com>
2024-12-31 18:50:55 +0100

    ajout du WIP

--- Dashborad_Course_Sportive.py
+++ Dashborad_Course_Sportive.py
@@ -4,4 +4,6 @@ import json
 import os
 
 def dashboard_sportif():
-    st.title("Course de rameur")
+   st.markdown("""<h1 style="color:#A53131;">Course de rameur</h1>""", unsafe_allow_html=True)
+
+   st.markdown("""<h3 style="color:#E77866;"> 🛠️ WORK IN PROGRESS 🛠️</h3>""", unsafe_allow_html=True)
